55431d32475bddbdecf7e26194afb58e3c34bc57
himiklab <himiklab@users.noreply.github.com>
2014-09-29 18:57:27 +0400

    code style fixes

--- FileNotFoundException.php
+++ FileNotFoundException.php
@@ -2,7 +2,7 @@
 /**
  * @link https://github.com/himiklab/yii2-easy-thumbnail-image-helper
  * @copyright Copyright (c) 2014 HimikLab
- * @license http://opensource.org/licenses/MIT
+ * @license http://opensource.org/licenses/MIT MIT
  */
 
 namespace himiklab\thumbnail;
